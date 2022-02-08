71c468337c49b758e922c7fd836517e3d24b2fda
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-08 10:47:37 +0200

    adding dummy data and form

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Yana", text: "Some text is the best!" },
+  { id: "q2", author: "Yoshi", text: "Bau Bau Mya Mya!" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
